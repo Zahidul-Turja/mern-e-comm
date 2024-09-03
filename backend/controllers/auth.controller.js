a97d4d92194f50be7ced2c9535ac592730a38d24
Zahidul-Turja <zahidulturja@gmail.com>
2024-09-03 22:24:35 +0600

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Sign up port");
+};
+
+export const login = async (req, res) => {
+  res.send("Login up port");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout up port");
+};
