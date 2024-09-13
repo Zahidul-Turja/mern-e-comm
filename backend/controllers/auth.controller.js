028eacee0be9f30c561d5058390e067c9f8af8f2
Zahidul-Turja <zahidulturja@gmail.com>
2024-09-13 14:15:23 +0600

    Login error handled

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,7 +88,7 @@ export const login = async (req, res) => {
         role: user.role,
       });
     } else {
-      res.status(401).json({ message: "Invalid email or password" });
+      res.status(400).json({ message: "Invalid email or password" });
     }
   } catch (error) {
     res
